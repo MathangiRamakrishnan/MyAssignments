f62f046a76dc029585a3a6f79d861a8f106de873
MathangiRamakrishnan <149134033+MathangiRamakrishnan@users.noreply.github.com>
2023-10-31 09:44:20 +0530

    Update IsPrimeNumber.java

--- week2/day1/IsPrimeNumber.java
+++ week2/day1/IsPrimeNumber.java
@@ -3,9 +3,22 @@ package week2.day1;
 public class IsPrimeNumber {
 
 	public static void main(String[] args) {
-		int n = 7;
-		 	 
-		 
+		
+		int num = 7;
+		boolean flag = false;
+		
+		for(int i=2 ; i<num ; i++) {
+			if(num%i == 0) {
+				flag = true;
+			}
+		}
+		
+		if(flag!= true) {
+			System.out.println("The number " + num +" is prime number" );
+			
+		} else {
+			System.out.println("The number " + num + " is not a prime number");
+		}
 		 
 
 	}
